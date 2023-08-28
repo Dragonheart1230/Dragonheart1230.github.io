48e8f80b59575d0fe167af6e3a6a4e872d8161f2
Rory Thomson <rthomson700@gmail.com>
2023-08-28 19:50:34 +1200

    Testing react routes

---
+++ src/Test.tsx
@@ -0,0 +1,3 @@
+export const Test = () => {
+    return (<h1>Testing 1,2,3</h1>)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Route, Link, BrowserRouter } from 'react-router-dom'
 import './index.css'
+import { Test } from './Test'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route path='/' >
+        <Link to='/Test'>Test</Link>
+      </Route>
+      <Route path='/Test' >
+        <Test />
+      </Route>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
