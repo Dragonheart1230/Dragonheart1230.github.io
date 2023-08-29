1dfb4b7bbd20b33c87ae581bd7800cbf2a0341a6
Rory Thomson <rthomson700@gmail.com>
2023-08-29 17:57:45 +1200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import react from '@vitejs/plugin-react'
 import replace from 'rollup-plugin-replace'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
+export default defineConfig(({ mode, command }) => {
+  const config = {
     plugins: [react(),
     replace({ "process.env.NODE_ENV": JSON.stringify(mode) })
     ],
-
+    base: './',
   }
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
+  return config
 
 })
