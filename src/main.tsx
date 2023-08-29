2c87292e4669e1ef397176ef3301311abc7a3168
Rory Thomson <rthomson700@gmail.com>
2023-08-29 18:03:54 +1200

    browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import { App } from './components/App'
 
@@ -8,8 +8,9 @@ import { App } from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    {/* //@ts-ignore */}
+    <BrowserRouter basename={process.env.NODE_ENV != "development" ? '/' : '/react-vite-gh-pages/'}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode >,
 )
