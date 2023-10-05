c633899a4575a5352c28f9307b556de82af0219c
Rory Thomson <rthomson700@gmail.com>
2023-10-05 21:04:16 +1300

    231

--- src/pages.tsx
+++ src/pages.tsx
@@ -1,17 +1,17 @@
-import { Home } from "./pages/Home"
-import { Gis } from "./pages/Gis"
-import { Three } from "./pages/Three"
-export const pages = [
-    {
-        name: 'HOME',
-        page: <Home />
-    },
-    {
-        name: 'GIS',
-        page: <Gis />
-    },
-    {
-        name: 'THREE.JS',
-        page: <Three />
-    },
-]
+// import { Home } from "./pages/Home"
+// import { Gis } from "./pages/Gis"
+// import { Three } from "./pages/Three"
+// export const pages = [
+//     {
+//         name: 'HOME',
+//         page: <Home />
+//     },
+//     {
+//         name: 'GIS',
+//         page: <Gis />
+//     },
+//     {
+//         name: 'THREE.JS',
+//         page: <Three />
+//     },
+// ]
